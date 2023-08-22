273fcfa18eee0ed7c03712e67c8dd04381801191
Ian Jones <jonesi_mail@yahoo.co.uk>
2023-08-22 19:56:56 +0100

    Stop double checking!

--- src/isValidSudoku.ts
+++ src/isValidSudoku.ts
@@ -29,40 +29,27 @@ function isValidCol(cellSet: Set<string>, board: string[][],colNum: number, star
   return true;
 }
 
-export function isValidBox(boardSet: Set<string>, cellSet: Set<string>, board: string[][], boxNum: number): boolean {
+export function isValidBox(cellSet: Set<string>, board: string[][], boxNum: number): boolean {
   const colStart = (boxNum < 3) ? boxNum * 3 : (boxNum < 6) ? (boxNum - 3) * 3 : (boxNum < 9) ? (boxNum - 6) * 3 : 0;
   const rowStart = (boxNum < 3) ? 0 : (boxNum < 6) ? 3 : (boxNum < 9) ? 6 : 0;
-  let isValid: boolean = true;
   // console.log(`box ${boxNum} - rowStart = ${rowStart}, colStart = ${colStart}`);
-  boardSet.clear();
+  cellSet.clear();
   for(let x = 0; x < 3; x++) {
     for (let y = 0; y < 3; y++) {
-      isValid = isValidRow(cellSet, board, rowStart + x, colStart + y)
-      if(isValid === false) {
-        break;
-      }
-      isValid = isValidCol(cellSet, board, rowStart + x, colStart + y)
-      if(isValid === false) {
-        break;
-      }
       if(board[rowStart + x][colStart + y] !== '.') {
-        if(boardSet.has(board[rowStart + x][colStart + y])) {
+        if(cellSet.has(board[rowStart + x][colStart + y])) {
           return false;
         } else {
-          boardSet.add(board[rowStart + x][colStart + y]);
+          cellSet.add(board[rowStart + x][colStart + y]);
         } 
       }
     }
-    if(isValid === false) {
-      break;
-    }
   }
-  return isValid;
+  return true;
 }
 
 export function isValidSudoku(board: string[][]): boolean {
   const cellSet: Set<string> = new Set();
-  const boardSet: Set<string> = new Set();
   let isValid: boolean = true;
   // console.log('Start')
   for(let i = 0; i < 9; i++) {
@@ -74,7 +61,7 @@ export function isValidSudoku(board: string[][]): boolean {
     if(isValid === false) {
       break;
     }
-    isValid = isValidBox(boardSet,cellSet, board, i);
+    isValid = isValidBox(cellSet, board, i);
     if(isValid === false) {
       break;
     }
