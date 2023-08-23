850a05fa47466fe33bc0febee09770fd4fc3ad14
Ian Jones <jonesi_mail@yahoo.co.uk>
2023-08-23 09:43:37 +0100

    Remove redundent params

--- src/isValidSudoku.ts
+++ src/isValidSudoku.ts
@@ -1,12 +1,10 @@
-export function isValidRow(cellSet: Set<string>, board: string[][], rowNum: number, startPos: number = 0, rowLen: number = 9): boolean {
-  // const cellSet = new Set();
+export function isValidRow(cellSet: Set<string>, board: string[][], rowNum: number): boolean {
   cellSet.clear();
-  for(let i = startPos; i < rowLen; i++) {
+  for(let i = 0; i < 9; i++) {
     if(board[rowNum][i] !== '.') {
       if(cellSet.has(board[rowNum][i])) {
         return false;
       } else {
-        // console.log(`Record R${board[rowNum][i]}`)
         cellSet.add(board[rowNum][i]);
       } 
     }
@@ -14,10 +12,9 @@ export function isValidRow(cellSet: Set<string>, board: string[][], rowNum: numb
   return true;
 }
 
-function isValidCol(cellSet: Set<string>, board: string[][],colNum: number, startPos: number = 0, colLen: number = 9): boolean {
-  // const cellSet = new Set();
+function isValidCol(cellSet: Set<string>, board: string[][],colNum: number): boolean {
   cellSet.clear();
-  for(let i = startPos; i < colLen; i++) {
+  for(let i = 0; i < 9; i++) {
     if(board[i][colNum] !== '.') {
       if(cellSet.has(board[i][colNum])) {
         return false;
@@ -32,7 +29,6 @@ function isValidCol(cellSet: Set<string>, board: string[][],colNum: number, star
 export function isValidBox(cellSet: Set<string>, board: string[][], boxNum: number): boolean {
   const colStart = (boxNum < 3) ? boxNum * 3 : (boxNum < 6) ? (boxNum - 3) * 3 : (boxNum < 9) ? (boxNum - 6) * 3 : 0;
   const rowStart = (boxNum < 3) ? 0 : (boxNum < 6) ? 3 : (boxNum < 9) ? 6 : 0;
-  // console.log(`box ${boxNum} - rowStart = ${rowStart}, colStart = ${colStart}`);
   cellSet.clear();
   for(let x = 0; x < 3; x++) {
     for (let y = 0; y < 3; y++) {
@@ -51,7 +47,6 @@ export function isValidBox(cellSet: Set<string>, board: string[][], boxNum: numb
 export function isValidSudoku(board: string[][]): boolean {
   const cellSet: Set<string> = new Set();
   let isValid: boolean = true;
-  // console.log('Start')
   for(let i = 0; i < 9; i++) {
     isValid = isValidRow(cellSet, board, i);
     if(isValid === false) {
@@ -65,7 +60,6 @@ export function isValidSudoku(board: string[][]): boolean {
     if(isValid === false) {
       break;
     }
-    // console.log(`i = ${i}, isValid = ${isValid}`);
   }
   return isValid;
 }
