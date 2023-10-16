683a62bdfa964aaab73fb5fb7adf0b27a6367ea7
Ian Jones <jonesi_mail@yahoo.co.uk>
2023-10-16 15:01:18 +0100

    2 ptr approach

--- src/two-pointers/twoSum2.ts
+++ src/two-pointers/twoSum2.ts
@@ -1,14 +1,15 @@
 export function twoSum2(numbers: number[], target: number): number[] {
-
-  const numMap = new Map<number,number>();
-
-  for(let i = 0; i < numbers.length; i++) {
-    if (numMap.has(numbers[i])) {
-      console.log([numMap.get(numbers[i]) || 0, i + 1]);
-      return [numMap.get(numbers[i]) || 0, i + 1];
-    } else {
-      numMap.set(target - numbers[i], (i + 1));
+  let lptr: number = 0;
+  let rptr: number = numbers.length - 1;
+  let result = null;
+  while(result !== target) {
+    result = numbers[lptr] + numbers[rptr];
+    if(result < target) {
+      lptr ++;
+    }
+    if(result > target) {
+      rptr --;
     }
   }
-  return [0,0];
+  return [lptr + 1, rptr + 1];
 }
